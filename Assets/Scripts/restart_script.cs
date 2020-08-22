4cf3cee9c7a01b04c992d51cef0aa4c2a7c03f2c
Leon <leon44ml@gmail.com>
2020-08-22 12:53:52 +0300

    Revert "Revert "All scripts now commented""
    This reverts commit 54befad78b487e67b875bbaf5824bb6d37502f16.

--- Assets/Scripts/restart_script.cs
+++ Assets/Scripts/restart_script.cs
@@ -3,16 +3,18 @@ using UnityEngine.SceneManagement;
 
 public class restart_script : MonoBehaviour
 {
-    public void Restart()
+    public void Restart() //restart current scene
     {
+        Time.timeScale = 1; //reset time scale, (restarting the scene while the game is paused will keep the timeScale value of 0)
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
-    public void StartGame(Scene scene)
+    public void StartGame(Scene scene) //start with passed scene name
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene(scene.name);
     }
-    public void PauseToggle()
+    public void PauseToggle() //Toggle pause
     {
         if (Time.timeScale == 1)
             Time.timeScale = 0;
